aafefc0360e89b87b967d2905417cc3966f782b7
yj-melissa <melimelame@gmail.com>
2022-11-16 23:06:46 +0900

    fetch

--- back-side/accounts/models.py
+++ back-side/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     nickname = models.CharField(max_length=50)
